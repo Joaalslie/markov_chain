215ff3b826998e58f1e296fa51d315f72d233218
joaalslie <jal029@post.uit.no>
2019-02-11 22:35:26 +0100

    split json-parsing into two functions, ready to create markov chain structure

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 #include "markov_chain.h"
 
+#define BUFFER_SIZE 2048
+
 void verify_matrix(int n_elems, int dimmensions, double *matrix){
 
     printf("Verifying markov chain/matrix..\n");
@@ -20,7 +22,7 @@ void verify_matrix(int n_elems, int dimmensions, double *matrix){
             for(j = i; j < (i + dimmensions); j++){
                 sum += matrix[j];
             }
-            // Need to compare, to a value close to zero, since it's hard to compare doubles..
+            // Need to compare, to a value close to zero, since it's hard to compare exact doubles..
             if((sum - 1.000) > 0.001 || (sum - 1.000) < -0.001){
                 printf("ERROR! The sum of a row in the matrix must always be 1.0!\n");
                 exit(0);
@@ -32,10 +34,9 @@ void verify_matrix(int n_elems, int dimmensions, double *matrix){
     printf("Matrix verified! no errors detected!\n");
 }
 
-int main(int argc, char **argv){
+void parse_matrix(char *filename, int dimmension_size, double *elements){
 
     struct json_object *parsed_json;
-    struct json_object *dimmensions;
     struct json_object *matrix;
     struct json_object *elem;
 
@@ -45,34 +46,61 @@ int main(int argc, char **argv){
     size_t i;
 
     FILE *fp;
-    char buffer[2048];
+    char buffer[BUFFER_SIZE];
 
-
-    if(argc < 2 && argc > 2){
-        printf("Include only one json file to parse as a chain as argument\n");
-        exit(0);
-    }
-
-    fp = fopen(argv[1], "r");
-    fread(buffer, 2048, 1, fp);
+    fp = fopen(filename, "r");
+    fread(buffer, BUFFER_SIZE, 1, fp);
     fclose(fp);
 
     parsed_json = json_tokener_parse(buffer);
 
-    json_object_object_get_ex(parsed_json, "num_dim", &dimmensions);
     json_object_object_get_ex(parsed_json, "matrix", &matrix);
 
-    dim_num = json_object_get_int(dimmensions);
-    double dim_num_d =json_object_get_double(dimmensions);
-
     n_elems = json_object_array_length(matrix);
-    double elements[n_elems];
+    //double elements[n_elems];
 
     for(i = 0; i < n_elems; i++){
         elem = json_object_array_get_idx(matrix, i);
         elements[i] = json_object_get_double(elem);
     }
+    printf("elements: %ld, dimmensions: %d \n", n_elems, dimmension_size);
+    verify_matrix((int)n_elems, dimmension_size, elements);    
+}
+
+int parse_matrix_dimmensions(char *filename){
+    struct json_object *parsed_json;
+    struct json_object *dimmensions;
+    char buffer[BUFFER_SIZE];
+    int dim_num_d;
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    fread(buffer, BUFFER_SIZE, 1, fp);
+    fclose(fp);
+
+    parsed_json = json_tokener_parse(buffer);
+    json_object_object_get_ex(parsed_json, "num_dim", &dimmensions);
+
+    dim_num_d = json_object_get_int(dimmensions);
+
+    return dim_num_d;
+}
+
+int main(int argc, char **argv){
+
+    //double *matrix;
+    int num_elems, dimmensions;
+
+    if(argc < 2 && argc > 2){
+        printf("Include only one json file to parse as a chain as argument\n");
+        exit(0);
+    }
+    dimmensions = parse_matrix_dimmensions(argv[1]);
+
+    num_elems = dimmensions * dimmensions;
+
+    double *matrix = malloc(sizeof(double) * num_elems);
     
-    verify_matrix((int)n_elems, (int)dim_num_d, elements);
+    parse_matrix(argv[1], dimmensions, matrix);
 
 }
