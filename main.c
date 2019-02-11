d43a9a8de30991e11f90d3f3432768459e6dd0f9
joaalslie <jal029@post.uit.no>
2019-02-11 21:47:43 +0100

    Worked on verify_matrix(), now actually working, i swear!

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void verify_matrix(int n_elems, int dimmensions, double *matrix){
 
     printf("Verifying markov chain/matrix..\n");
     int i, j;
-    double sum = 0;
+    double sum = 0.0;
     int d = dimmensions * dimmensions;
 
     if(n_elems != d){
@@ -20,11 +20,12 @@ void verify_matrix(int n_elems, int dimmensions, double *matrix){
             for(j = i; j < (i + dimmensions); j++){
                 sum += matrix[j];
             }
-            if(sum != 1){
-                printf("ERROR! The sum of a row int he matrix must always be zero!\n");
+            // Need to compare, to a value close to zero, since it's hard to compare doubles..
+            if((sum - 1.000) > 0.001 || (sum - 1.000) < -0.001){
+                printf("ERROR! The sum of a row in the matrix must always be 1.0!\n");
                 exit(0);
             } else{
-                sum = 0;
+                sum = 0.0;
             }
         }
     }
@@ -32,18 +33,20 @@ void verify_matrix(int n_elems, int dimmensions, double *matrix){
 }
 
 int main(int argc, char **argv){
-    FILE *fp;
-    char buffer[2048];
 
     struct json_object *parsed_json;
     struct json_object *dimmensions;
-    struct json_object *rows;
+    struct json_object *matrix;
     struct json_object *elem;
 
     int dim_num;
 
     size_t n_elems;
     size_t i;
+
+    FILE *fp;
+    char buffer[2048];
+
 
     if(argc < 2 && argc > 2){
         printf("Include only one json file to parse as a chain as argument\n");
@@ -57,24 +60,19 @@ int main(int argc, char **argv){
     parsed_json = json_tokener_parse(buffer);
 
     json_object_object_get_ex(parsed_json, "num_dim", &dimmensions);
-    json_object_object_get_ex(parsed_json, "rows", &rows);
+    json_object_object_get_ex(parsed_json, "matrix", &matrix);
 
     dim_num = json_object_get_int(dimmensions);
     double dim_num_d =json_object_get_double(dimmensions);
 
-    n_elems = json_object_array_length(rows);
+    n_elems = json_object_array_length(matrix);
     double elements[n_elems];
 
-    printf("dimmension size: %d\n", dim_num);
-
     for(i = 0; i < n_elems; i++){
-        elem = json_object_array_get_idx(rows, i);
+        elem = json_object_array_get_idx(matrix, i);
         elements[i] = json_object_get_double(elem);
     }
-
-    for(int j = 0; j < n_elems; j++){
-        printf("%d : %f\n", j+1, elements[j]);
-    }
+    
     verify_matrix((int)n_elems, (int)dim_num_d, elements);
 
 }
