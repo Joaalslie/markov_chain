c02b9168a95becaba78a8be90568772da0bd8bd9
joaalslie <jal029@post.uit.no>
2019-06-01 18:54:36 +0200

    Adds main loop, still need to fix parser

--- weather.c
+++ weather.c
@@ -6,9 +6,28 @@
 #include "markov_chain.h"
 #include "json_parser.h"
 
+void parse(char *buffer, int val)
+{
+    switch(val)
+    {
+        case 0:
+            // Weather is sunny.
+
+        case 1:
+            // Weather is cloudy.
+
+        case 2:
+            // Weather is rainy.
+    }   
+}
+
 int main(int argc, char **argv)
 {
     int num_elems, dimmensions, start_node;
+    discrete_chain_t *chain;
+    char buffer[20];
+    float *matrix;
+    char scan;
     time_t t;
 
     if (argc < 2 || argc > 2)
@@ -26,15 +45,28 @@ int main(int argc, char **argv)
     start_node = parse_start_node(argv[1]);
 
     // Allocate memory for the array/matrix
-    float *matrix = malloc(sizeof(float) * num_elems);
+    matrix = malloc(sizeof(float) * num_elems);
 
     // Parse the json-file, adding the elements from the
     // array in the file, to the newly allocated array/matrix
     parse_matrix(argv[1], dimmensions, matrix);
 
-    discrete_chain_t *chain = chain_create(matrix, dimmensions, start_node);
+    chain = chain_create(matrix, dimmensions, start_node);
 
     // initiate randomization
     srand((unsigned long)time(&t));
 
+    printf("To exit, type: X\n");
+    parse(buffer, get_current_node(chain));
+    printf("Initially, the weather is: %s\n", buffer);
+    while(1)
+    {
+        printf("Press ENTER to transition..\n");
+        scanf("%c", scan);
+        if(scan == 'X')
+            break;
+        transition(chain);
+        parse(buffer, get_current_node(chain));
+        printf("The weather today is: %s\n", buffer);
+    }
 }
